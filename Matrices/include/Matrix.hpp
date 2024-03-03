8d3789cea79cbd2e736aad6bd01c57f930b01555
Divinecrusade <gorlovdima@inbox.ru>
2024-03-03 12:30:35 +0300

    refactor: Replace rows and cols fix: Get transpered, determinant, inverse matrix

--- Matrices/include/Matrix.hpp
+++ Matrices/include/Matrix.hpp
@@ -22,7 +22,7 @@ namespace Matrices
     template<size_t nCols, size_t nRows>
     concept SquareSizes = nCols == nRows;*/
 
-    template<size_t nCols, size_t nRows, typename T = double>
+    template<size_t nRows, size_t nCols, typename T = double>
     class Matrix
     {
     private:
@@ -85,28 +85,28 @@ namespace Matrices
             return data.cend();
         }
         
-        std::array<T, nRows> const& operator[](size_t i) const
+        std::array<T, nCols> const& operator[](size_t i) const
         {
-            assert(i <= nCols);
+            assert(i <= nRows);
 
             return data[i];
         }
-        std::array<T, nRows>& operator[](size_t i)
+        std::array<T, nCols>& operator[](size_t i)
         {
-            assert(i <= nCols);
+            assert(i <= nRows);
 
             return data[i];
         }
 
-        Matrix<nCols, nRows, T> operator!() const noexcept
+        Matrix operator!() const noexcept
         {
             Matrix tmp{ };
 
             double const D{ get_determinant() };
             assert(!is_equal_with_precision(D, 0));
 
-            for (size_t i{ 0U }; i != nCols; ++i)
-            for (size_t j{ 0U }; j != nRows; ++j)
+            for (size_t i{ 0U }; i != nRows; ++i)
+            for (size_t j{ 0U }; j != nCols; ++j)
             {
                 tmp[i][j] = get_co_factor(i, j) / D;
             }
@@ -114,84 +114,82 @@ namespace Matrices
             return tmp.get_transpered();
         }
 
-        Matrix<nCols, nRows, T>& operator+=(Matrix<nCols, nRows, T> const& rhs) noexcept
+        Matrix& operator+=(Matrix const& rhs) noexcept
         {
-            std::transform(data.begin(), data.end(), rhs.begin(), data.begin(),
+            std::transform((*this).begin(), (*this).end(), rhs.begin(), data.begin(),
             [](auto const& col1, auto const& col2)
             {
-                std::array<T, nRows> new_col{ };
+                std::array<T, nCols> new_row{ };
 
-                std::transform(col1.begin(), col1.end(), col2.begin(), new_col.begin(), std::plus<T>{});
+                std::transform(col1.begin(), col1.end(), col2.begin(), new_row.begin(), std::plus<T>{});
 
-                return new_col;
+                return new_row;
             });
 
             return *this;
         }
-        Matrix<nCols, nRows, T>& operator-=(Matrix<nCols, nRows, T> const& rhs) noexcept
+        Matrix& operator-=(Matrix const& rhs) noexcept
         {
             std::transform(data.begin(), data.end(), rhs.begin(), data.begin(),
             [](auto const& col1, auto const& col2)
             {
-                std::array<T, nRows> new_col{ };
+                std::array<T, nCols> new_row{ };
 
-                std::transform(col1.begin(), col1.end(), col2.begin(), new_col.begin(), std::minus<T>{});
+                std::transform(col1.begin(), col1.end(), col2.begin(), new_row.begin(), std::minus<T>{});
 
-                return new_col;
+                return new_row;
             });
 
             return *this;
         }
 
-        std::array<std::array<T, nCols>, nRows> get_transpered() const noexcept
+        Matrix<nCols, nRows, T> get_transpered() const noexcept
         {
-            std::array<std::array<T, nCols>, nRows> tmp{ };
+            Matrix<nCols, nRows, T> transpered{ };
 
-            for (size_t i{ 0U }; i != nRows; ++i)
-            for (size_t j{ 0U }; j != nCols; ++j)
+            for (size_t i{ 0U }; i != nCols; ++i)
+            for (size_t j{ 0U }; j != nRows; ++j)
             {
-                tmp[i][j] = data[j][i];
+                transpered[i][j] = data[j][i];
             }
 
-            return tmp;
+            return transpered;
         }
         double get_determinant() const noexcept
         {
             double D{ 1. };
-            Matrix B{ *this };
+            Matrix triangled{ *this };
 
-            for (size_t I{ 0U }; I != nCols - 1U; ++I)
+            for (size_t I{ 0U }; I != nRows - 1U; ++I)
             {
-                if (!(is_equal_with_precision(B[I][I], 0.)))
+                if (is_equal_with_precision(triangled[I][I], 0.))
                 {
-                    size_t i{ };
-                    for (i = I + 1U; i != nCols; ++i)
-                      if (is_equal_with_precision(B[i][I])) break;
-
-
-                    for (size_t j{ 0U }; j != nRows; ++j)
+                    for (size_t i{ I + 1U }; i != nRows; ++i)
                     {
-                        std::swap(B[I][j], B[i][j]);
+                        if (!is_equal_with_precision(triangled[i][I], 0.))
+                        {
+                            std::swap(triangled[I], triangled[i]);
+
+                            break;
+                        }
                     }
 
                     D *= -1.;
                 }
 
-                for (size_t i{ I + 1U }; i != nCols; ++i)
+                for (size_t i{ I + 1U }; i != nRows; ++i)
                 {
-                    T const k{ -(B[i][I] / B[I][I]) };
+                    if (is_equal_with_precision(triangled[I][I], 0.)) return 0.;
 
-                    for (size_t R{ I }; R != nRows; ++R)
-                    {
-                        B[i][R] += B[I][R] * k;
-                    }
+                    T const k{ -(triangled[i][I] / triangled[I][I]) };
+
+                    for (size_t R{ I }; R != nCols; ++R)
+                        triangled[i][R] += triangled[I][R] * k;
                 }
             }
 
-            for (size_t i{ 0U }; i != nCols; ++i)
-            {
-                D *= B[i][i];
-            }
+            for (size_t i{ 0U }; i != nRows; ++i)
+                D *= triangled[i][i];
 
             return D;
         }
@@ -200,16 +198,16 @@ namespace Matrices
 
         T get_co_factor(size_t I, size_t J) const noexcept
         {
-            Matrix<(nCols - 1U != 0U ? nCols - 1U : nCols), (nRows - 1U != 0U ? nRows - 1U : nRows), T> minor_matrix{ };
+            Matrix<(nRows - 1U != 0U ? nRows - 1U : nRows), (nCols - 1U != 0U ? nCols - 1U : nCols), T> minor_matrix{ };
 
-            for (size_t i1{ 0U }, i2{ 0U }; i2 < nCols - 1U; ++i1, ++i2)
+            for (size_t i1{ 0U }, i2{ 0U }; i2 < nRows - 1U; ++i1, ++i2)
             {
                 if (i1 == I) ++i1;
 
-                for (size_t j1{ 0U }, j2{ 0U }; j2 < nRows - 1U; ++j1, ++j2)
+                for (size_t j1{ 0U }, j2{ 0U }; j2 < nCols - 1U; ++j1, ++j2)
                 {
                     if (j1 == J) ++j1;
-                    minor_matrix[i2][j2] = *this[i1][j1];
+                    minor_matrix[i2][j2] = (*this)[i1][j1];
                 }
             }
 
@@ -218,30 +216,30 @@ namespace Matrices
     };
 
 
-    template<size_t nCols, size_t nRows, typename T>
-    Matrix<nCols, nRows, T> operator+(Matrix<nCols, nRows, T> const& lhs, Matrix<nCols, nRows, T> const& rhs) noexcept
+    template<size_t nRows, size_t nCols, typename T>
+    Matrix<nRows, nCols, T> operator+(Matrix<nRows, nCols, T> const& lhs, Matrix<nRows, nCols, T> const& rhs) noexcept
     {
-        return Matrix<nCols, nRows, T>{ lhs } += rhs;
+        return Matrix{ lhs } += rhs;
     }
 
-    template<size_t nCols, size_t nRows, typename T>
-    Matrix<nCols, nRows, T> operator-(Matrix<nCols, nRows, T> const& lhs, Matrix<nCols, nRows, T> const& rhs) noexcept
+    template<size_t nRows, size_t nCols, typename T>
+    Matrix<nRows, nCols, T> operator-(Matrix<nRows, nCols, T> const& lhs, Matrix<nRows, nCols, T> const& rhs) noexcept
     {
-        return Matrix<nCols, nRows, T>{ lhs } -= rhs;
+        return Matrix{ lhs } -= rhs;
     }
 
-    template<size_t nCols, size_t nCols2, size_t nRows, typename T>
-    Matrix<nCols, nRows, T> operator*(Matrix<nCols, nRows, T> const& lhs, Matrix<nCols2, nCols, T> const& rhs) noexcept
+    template<size_t nRows1, size_t nCols1, size_t nCols2, typename T>
+    Matrix<nRows1, nCols2, T> operator*(Matrix<nRows1, nCols1, T> const& lhs, Matrix<nCols1, nCols2, T> const& rhs) noexcept
     {
-        Matrix<nCols, nRows, T> result{ };
+        Matrix<nRows1, nCols2, T> result{ };
 
-        for (size_t i{ 0U }; i != nCols; ++i)
-        for (size_t j{ 0U }; j != nRows; ++j)
+        for (size_t i{ 0U }; i != nRows1; ++i)
+        for (size_t j{ 0U }; j != nCols2; ++j)
         {
             result[i][j] = static_cast<T>(0);
 
-            for (size_t k{ 0U }; k != nCols2; ++k)
-                result += lhs[i][k] * rhs[k][j];
+            for (size_t k{ 0U }; k != nCols1; ++k)
+                result[i][j] += lhs[i][k] * rhs[k][j];
         }
 
         return result;
--- Matrices/src/main.cpp
+++ Matrices/src/main.cpp
@@ -5,24 +5,49 @@
 
 using namespace Matrices;
 
-template<size_t nCols, size_t nRows, typename T>
-void print_matrix(Matrix<nCols, nRows, T> const& matrix)
+template<size_t nRows, size_t nCols, typename T>
+void print_matrix(Matrix<nRows, nCols, T> const& matrix)
 {
-    for (auto const& col : matrix)
+    for (auto const& row : matrix)
     {
-        std::copy(col.begin(), col.end(), std::ostream_iterator<T>{std::cout, " "});
+        std::copy(row.begin(), row.end(), std::ostream_iterator<T>{std::cout, " "});
         std::cout << "\n";
     }
 }
 
 int main()
 {
-    Matrix<3, 1> M1{ };
-    Matrix<3, 1> M2{ {1.0, 2.0, 3.0} };
+    Matrix<1, 3> M1{ };
+    Matrix<1, 3> M2{ {1.0, 2.0, 3.0} };
+    Matrix<1, 3> M3{ M2 };
 
     print_matrix(M1);
     std::cout << "\n";
     print_matrix(M2);
+    std::cout << "\n";
+    print_matrix(M3);
+    std::cout << "\n";
+    M3 += M2;
+    print_matrix(M3);
+    std::cout << "\n";
+    print_matrix(M2 + M3);
+    std::cout << "\n";
+    print_matrix(M3 - M2);
+    std::cout << "\n";
+    print_matrix(M3.get_transpered());
+    std::cout << "\n";
+    auto M4{ M3.get_transpered() * M3 };
+    print_matrix(M4);
+    std::cout << "\n";
+    std::cout << M4.get_determinant();
+    std::cout << "\n" << "\n";
+    M4 = Matrix<3U, 3U>{ { 3., 1., 2., -4., 4., 1., 0., 2., 3. } };
+    print_matrix(M4);
+    std::cout << "\n" << M4.get_determinant() << "\n" << "\n";
+    auto M5{ !M4 };
+    print_matrix(M5);
+    std::cout << "\n";
+    print_matrix(M4 * M5);
 
     return 0;
 }
